f4ebd3e2f54cce09cc0f6b60781a4852a61bd332
wenbo <18629531510@163.com>
2020-10-26 18:16:47 +0800

    生成stories

---
+++ storybook-static/precache-manifest.e8e603f02c9d5bce874c7f74afea01a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab0428d25c88fe8d3f6b491c69a19d1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a2bb7100eb2c3f5c9386.bundle.js"
+  },
+  {
+    "url": "runtime~main.a2bb7100eb2c3f5c9386.bundle.js"
+  },
+  {
+    "revision": "7db1f9d95b8f4f1655e3",
+    "url": "static/css/main.55c1ead1.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.a2bb7100eb2c3f5c9386.bundle.js"
+  },
+  {
+    "url": "vendors~main.a2bb7100eb2c3f5c9386.bundle.js.LICENSE.txt"
+  }
+]);
