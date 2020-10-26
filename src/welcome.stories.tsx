9ac50b82dcb7360e6f8613b93f64069ea1adc93e
wenbo <18629531510@163.com>
2020-10-26 17:43:15 +0800

    修改stories

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -10,14 +10,13 @@ const welcome = () => {
   return (
     <>
       <div style={ titleStyle }>
-        <h2>欢迎来到 vikingship 组件库</h2>
+        <h2>欢迎来到 vikiship 组件库</h2>
       </div>
       <h3>使用 REACT+TYPESCRIPT 从零到一打造一套你自己的组件库</h3>
 
-
       <h3>安装试试</h3>
       <code>
-        npm install vikingship --save
+        npm install vikiship --save
         </code>
     </>
   )
