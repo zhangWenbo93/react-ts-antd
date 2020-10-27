68014866756c4be33f1e647d82ebd9c3ee8d469e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-27 12:10:57 +0000

    Deploy zhangWenbo93/react-ts-antd to github.com/zhangWenbo93/react-ts-antd.git:gh-pages

---
+++ precache-manifest.e6ddb44aadced0ce26a608f2e2d64d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c089e24ddc04e25fc08fc41a9ea84f1b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.448506026f0a17966753.bundle.js"
+  },
+  {
+    "url": "runtime~main.448506026f0a17966753.bundle.js"
+  },
+  {
+    "revision": "baa4e3e0ffcf8f1c9f06",
+    "url": "static/css/main.55c1ead1.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.448506026f0a17966753.bundle.js"
+  },
+  {
+    "url": "vendors~main.448506026f0a17966753.bundle.js.LICENSE.txt"
+  }
+]);
